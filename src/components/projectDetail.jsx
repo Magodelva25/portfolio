c8b59f84dca761f16568fffb0db7e750fd070b81
Magodelva25 <migudz@gmail.com>
2025-03-04 14:34:47 +0100

    adding handlers

--- src/components/projectDetail.jsx
+++ src/components/projectDetail.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import info from "../info.json";
 import TagBadge from "./tagBadge";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const ProjectDetail = () => {
   const { projectName } = useParams();
@@ -13,35 +15,110 @@ const ProjectDetail = () => {
   if (!project) {
     return <h2>Proyecto no encontrado</h2>;
   }
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 800);
+  };
+  const hiddenContent = () => {
+    setIsOpen(false);
+  };
+  const showingContent = () => {
+    setIsOpen(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <div className="project-detail">
-      <div className="visual-carousel">
-        {project.projectMedia?.map((object, index) => (
-          <img loading="lazy" className="visual-carousel-img" key={index} src={object.url} />
-        ))}
-      </div>
-      <div className="project-content">
-        <div className="project-title">
-          <h2>{project.name}</h2>
-          <p>{project.year}</p>
+    <>
+      {isMobile ? (
+        <div className="project-detail">
+          <div className="visual-carousel">
+            <div className="project-carousel-card">
+              {project.projectMedia?.map((object, index) => (
+                <div
+                  className="visual-carousel-img"
+                  style={{ backgroundImage: `url(${object.url})` }}
+                  key={index}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="project-content">
+            <div className="project-title">
+              <h2>{project.name}</h2>
+              <p>{project.year}</p>
+            </div>
+            <div className="project-description">
+              <div className="description-header">
+                <div className="project-tags">
+                  {project.keywords?.map((keyword, i) => (
+                    <TagBadge key={`${project.name}-${i}`} content={keyword} />
+                  ))}
+                </div>
+                <div className="important-data">
+                  <p>{project.client}</p>
+                  <p>{project.location}</p>
+                </div>
+              </div>
+              <p>{project.description}</p>
+            </div>
+          </div>
         </div>
-        <div className="project-description">
-        <div className="description-header">
-          <div className="project-tags">
-            {project.keywords?.map((keyword, i) => (
-              <TagBadge key={`${project.name}-${i}`} content={keyword} />
-            ))}
+      ) : (
+        <div className={`project-detail${!isOpen ? "-hidden" : ""}`}>
+          <div className="visual-carousel">
+            <div className="project-carousel-card">
+              {project.projectMedia?.map((object, index) => (
+                <div
+                  className="visual-carousel-img"
+                  style={{ backgroundImage: `url(${object.url})` }}
+                  key={index}
+                />
+              ))}
+            </div>
           </div>
-          <div className="important-data">
-            <p>{project.client}</p>
-            <p>{project.location}</p>
-          </div>
-          </div>
-          <p>{project.description}</p>
+          {isOpen ? (
+            <div className="project-content">
+              <div className="project-title">
+                <div className="close-handler" onClick={hiddenContent}>
+                  <ArrowForwardIosIcon className="close-icon" />
+                </div>
+                <h2>{project.name}</h2>
+                <p>{project.year}</p>
+              </div>
+              <div className="project-description">
+                <div className="description-header">
+                  <div className="project-tags">
+                    {project.keywords?.map((keyword, i) => (
+                      <TagBadge
+                        key={`${project.name}-${i}`}
+                        content={keyword}
+                      />
+                    ))}
+                  </div>
+                  <div className="important-data">
+                    <p>{project.client}</p>
+                    <p>{project.location}</p>
+                  </div>
+                </div>
+                <p>{project.description}</p>
+              </div>
+            </div>
+          ) : (
+            <div className="handler-content" onClick={showingContent}>
+              <span>Open info</span>
+              <ArrowBackIosIcon className="content-icon" />
+            </div>
+          )}
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
