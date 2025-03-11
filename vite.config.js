2c2895e746e18d906fb28dc658cb4185421d9ed3
Magodelva25 <migudz@gmail.com>
2025-03-12 00:47:22 +0100

    yaml

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173
   },
-  base: '/', // Asegúrate de que la base esté configurada correctamente si despliegas en un subdirectorio
-});
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173
+  }
+};
