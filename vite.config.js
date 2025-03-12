8ae98141c6781ffee59bfc2be74067726a4c2434
Magodelva25 <migudz@gmail.com>
2025-03-12 01:20:09 +0100

    yaml

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+    external: [],
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
-    strictPort: true
+    strictPort: true,
   },
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
     strictPort: true,
-    allowedHosts: ['mango-portfolio-9tvc.onrender.com']
-  }
-};
+    allowedHosts: ['mango-portfolio-9tvc.onrender.com'],
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
