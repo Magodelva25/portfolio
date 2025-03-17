37d50141c8676a860efda5a98e7d1f6a63727421
Magodelva25 <migudz@gmail.com>
2025-03-17 22:23:50 +0100

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './styles/App.css'
 import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
