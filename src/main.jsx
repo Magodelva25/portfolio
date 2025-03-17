c46d9cc7b162ee244fbb3a8fed00cd45d22c0b5b
Magodelva25 <migudz@gmail.com>
2025-03-17 22:19:52 +0100

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './styles/App.css'
 import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Analytics/>
+    <SpeedInsights/>
   </React.StrictMode>
 );
